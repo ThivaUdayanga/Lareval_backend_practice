6658e67ac4b7de4b56bbe43a602e894e3a3ef31f
Thivanka <thivanka.wimalasena@gmail.com>
2026-01-22 23:08:53 +0530

    Routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/greating', function(){
+    return 'Hello world';
+});
+
+Route::post('/greating', function() {
+    return "Post Request sent";
+});
+
+Route::redirect('/welcome', '/greating', 301);
+
+$context = ['name'=>'Thivanka', 'age'=>25];
+Route::view('/myview', 'sample', $context);
