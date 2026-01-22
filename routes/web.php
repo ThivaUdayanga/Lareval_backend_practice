c0939a1178278d02423f11d9b4f5ec12ace94a2e
Thivanka <thivanka.wimalasena@gmail.com>
2026-01-22 23:57:05 +0530

    Route Lesson over

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,20 @@ Route::redirect('/welcome', '/greating', 301);
 
 $context = ['name'=>'Thivanka', 'age'=>25];
 Route::view('/myview', 'sample', $context);
+
+Route::get('/optional/{name?}', function (?string $name = 'Guest') {
+    return 'Hello '. $name;
+});
+
+// Route Regular Expression Constraints
+Route::get('/user/{name}', function (string $name) {
+    return 'User '.$name;
+})->where('name', '[A-Za-z]+');
+ 
+Route::get('/user/{id}', function (string $id) {
+    return 'User Id '.$id;
+})->where('id', '[0-9]+');
+ 
+Route::get('/user/{id}/{name}', function (string $id, string $name) {
+    return 'User Id '.$id.' User Name '.$name;
+})->where(['id' => '[0-9]+', 'name' => '[A-Za-z]+']);
